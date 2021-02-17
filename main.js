196111b7cee70a76fc0f23147b45c5e3f3a38b3c
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-16 19:16:06 -0600

    added json manifest

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /* This selects all the items on the YouTube homepage
 and deletes it */
-const content = document.getElementById("content");
-content.parentNode.removeChild(content);
 
+// const content = document.getElementById("content");
+// content.parentNode.removeChild(content);
+
+const content = $('#content')
+// const contentParent = content.Parent()
+// $("#content").prependChild(img.src = '/background.jpg');
+// $(`<img src = '${/background.jpg}'>`).prependTo(#'content')
+$('#content').replaceWith("<p>Hello!</p>", "<img id='theImg' src = '/background.jpg'/>");
+// content.remove()
