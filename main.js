1da3b7484a6dfac28078bb0b6298a685a6ac59dc
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-18 19:33:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ and deletes it */
 // content.parentNode.removeChild(content);
 
 const content = $('#content')
-/*function image() {
+/* function image() {
     // dynamically add an image and set its attribute
     const img = document.createElement('img')
     img.src = '/images/background.jpg'
@@ -15,7 +15,7 @@ const content = $('#content')
 // const contentParent = content.Parent()
 // $("#content").prependChild(img.src = '/background.jpg');
 // $(`<img src = '${/background.jpg}'>`).prependTo(#'content')
-const img = 'images/background.jpg';
-//$('#content').html(`<img src='${img}'>`).prepend('<h1>Hello!</h1>');
-$('#content').replaceWith(`<img src='images/background.jpg' width = '100' height = '500'>`).prepend('<h1>Let\'s get back to work!</h1>');
+const img = URL('https://unsplash.com/photos/IuSemNxGS88');
+$('#content').html(`<img src='${img}'>`).prepend('<h1>Let\'s get back to work!</h1>')
+//$('#content').replaceWith(`<img src='images/background.jpg' width = '100' height = '500'>`).prepend('<h1>Let\'s get back to work!</h1>');
 //content.remove()
